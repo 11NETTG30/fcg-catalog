f040d3c996a860f2d95911a2dfe329512cbab82f
gaabrielalex <gabrielalexsilva2@gmail.com>
2026-03-08 13:15:07 -0400

    ✨ feat(infrastructure): adiciona configuração de banco de dados da infraestrutura

---
+++ src/FCGCatalog.Infrastructure/Configurations/DatabaseConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using FCGCatalog.Infrastructure.Persistence;
+using FCGCatalog.Infrastructure.Persistence.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FCGCatalog.Infrastructure.Configurations;
+
+public static class DatabaseConfiguration
+{
+	extension(IServiceCollection services)
+	{
+		public void AddDatabase(IConfiguration configuration)
+		{
+			string? connectionString = configuration.GetConnectionString("DefaultConnection");
+
+			services.AddSingleton<AuditoriaSaveChangesInterceptor>();
+			services.AddDatabasePostgreSQL<CatalogoDbContext>(connectionString!);
+		}
+
+		private void AddDatabasePostgreSQL<T>(string connectionString) where T : DbContext
+		{
+			services.AddDbContext<T>((serviceProvider, options) =>
+				options
+					.UseNpgsql(connectionString, optionsPostgress =>
+					{
+						optionsPostgress.MigrationsHistoryTable("__ef_migrations_history");
+					})
+					.AddInterceptors(serviceProvider.GetRequiredService<AuditoriaSaveChangesInterceptor>())
+			);
+		}
+	}
+}
