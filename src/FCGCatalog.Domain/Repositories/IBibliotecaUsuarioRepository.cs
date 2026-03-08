532cdd71fef0df42df361283a1c75280e2d3294c
gaabrielalex <gabrielalexsilva2@gmail.com>
2026-03-08 17:18:03 -0400

    ✨ feat(repository): adiciona repositórios de Jogo e BibliotecaUsuario

---
+++ src/FCGCatalog.Domain/Repositories/IBibliotecaUsuarioRepository.cs
@@ -0,0 +1,8 @@
+﻿using FCGCatalog.Domain.Entities;
+using FCGCatalog.Domain.Shared.Abstractions;
+
+namespace FCGCatalog.Domain.Repositories;
+
+public interface IBibliotecaUsuarioRepository : IRepository<BibliotecaUsuario>
+{
+}
