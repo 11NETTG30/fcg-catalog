0d9319f68a882f70cf32c532806c915fc2822a20
Saulo Gustavo <70772825+Saulo0100@users.noreply.github.com>
2026-03-08 14:17:14 -0300

    criacao de domain

---
+++ src/FCGCatalog.Domain/Repositories/IRefreshTokenRepository.cs
@@ -0,0 +1,11 @@
+﻿using FCGCatalog.Domain.Entities;
+
+namespace FCGCatalog.Domain.Repositories;
+
+public interface IRefreshTokenRepository : IRepository<RefreshToken>
+{
+    Task<RefreshToken?> ObterPorId(Guid id);
+    Task<RefreshToken?> ObterPorToken(Guid refreshToken);
+    Task Adicionar(RefreshToken refreshToken);
+    Task<List<RefreshToken>> ListarNaoRevogadosPorUsuario(Guid usuarioId);
+}
---
+++ src/FCGCatalog.Domain/Services/RefreshTokenDomainService.cs
@@ -0,0 +1,8 @@
+﻿using FCGCatalog.Domain.Entities;
+
+namespace FCGCatalog.Domain.Services;
+
+public interface IRefreshTokenDomainService
+{
+    Task RevogarCadeiaDescendente(RefreshToken refreshToken, Guid refreshTokenId);
+}
