c5f32263cab60addfe526b3f4269d77e6eee7bc6
gaabrielalex <gabrielalexsilva2@gmail.com>
2026-03-10 21:42:26 -0400

    ✨ feat(repository): implementa operações do JogoRepository
    - Adiciona método Adicionar utilizando DbSet
    - Implementa verificação de existência por título
    - Inicializa DbSet<Jogo> no repositório
    - Ajusta método Dispose do DbContext

--- src/FCGCatalog.Infrastructure/Persistence/Repositories/JogoRepository.cs
+++ src/FCGCatalog.Infrastructure/Persistence/Repositories/JogoRepository.cs
@@ -1,30 +1,38 @@
 ﻿using FCGCatalog.Domain.Entities;
 using FCGCatalog.Domain.Repositories;
 using FCGCatalog.Domain.Shared.Uow;
+using Microsoft.EntityFrameworkCore;
 
 namespace FCGCatalog.Infrastructure.Persistence.Repositories;
 
 public sealed class JogoRepository : IJogoRepository
 {
 	private readonly CatalogoDbContext _dbContext;
+	private readonly DbSet<Jogo> _dbSet;
 	public IUnitOfWork UnitOfWork => _dbContext;
 
 	public JogoRepository(CatalogoDbContext dbContext)
 	{
 		_dbContext = dbContext;
+		_dbSet = _dbContext.Set<Jogo>();
 	}
+
+    public async Task Adicionar(Jogo jogo)
+    {
+		await _dbSet.AddAsync(jogo);
+	}
+
+	public async Task<bool> ExistePorTitulo(string titulo)
+	{
+		string tituloNormalizado = titulo.Trim().ToLower();
+
+		return await _dbSet.AnyAsync(j =>
+			j.Titulo.Trim().ToLower() == tituloNormalizado);
+	}
+
 	public void Dispose()
 	{
 		_dbContext?.Dispose();
 	}
 
-    public Task Adicionar(Jogo jogo)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<bool> ExistePorTitulo(string titulo)
-    {
-        throw new NotImplementedException();
-    }
 }
