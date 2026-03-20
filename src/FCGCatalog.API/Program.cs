27a66c7aff1e3441f03f176afa4db5ea6e702b43
Lennon Marconato <lennon_marconato@hotmail.com>
2026-03-20 16:14:03 -0300

    ✨ feat: integrar OpenTelemetry via FCG.Shared.Infrastructure
    Adiciona FCG.Shared.Infrastructure v1.1.0 e chamada a
    builder.AddObservabilidade() no Program.cs para habilitar
    tracing, métricas e logs via OTLP.

--- src/FCGCatalog.API/Program.cs
+++ src/FCGCatalog.API/Program.cs
@@ -1,4 +1,5 @@
-﻿using FCGCatalog.API.Configurations;
+﻿using FCG.Shared.Infrastructure.Configurations;
+using FCGCatalog.API.Configurations;
 using FCGCatalog.API.Middlewares;
 using FCGCatalog.Infrastructure.Configurations;
 using FCGCatalog.IoC;
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.RegisterIoCConfigurations();
 
 builder.AddLoggingConfiguration();
+builder.AddObservabilidade();
 builder.Services.AddControllersConfiguration();
 builder.Services.AddDocumentation();
 builder.Services.AddProblemDetailsConfiguration();
