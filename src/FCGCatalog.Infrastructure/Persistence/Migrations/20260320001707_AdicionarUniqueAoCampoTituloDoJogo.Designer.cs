f04a48b7f84800137e4bcbd992f3be3a08de5c12
gaabrielalex <gabrielalexsilva2@gmail.com>
2026-03-19 20:22:54 -0400

    Implementar indice unique para a coluna titulo do jogo

--- src/FCGCatalog.Infrastructure/Persistence/Migrations/20260320001707_AdicionarUniqueAoCampoTituloDoJogo.Designer.cs
+++ src/FCGCatalog.Infrastructure/Persistence/Migrations/20260320001707_AdicionarUniqueAoCampoTituloDoJogo.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FCGCatalog.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FCGCatalog.Infrastructure.Migrations
 {
     [DbContext(typeof(CatalogoDbContext))]
-    partial class CatalogoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260320001707_AdicionarUniqueAoCampoTituloDoJogo")]
+    partial class AdicionarUniqueAoCampoTituloDoJogo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/FCGCatalog.Infrastructure/Persistence/Migrations/20260320001707_AdicionarUniqueAoCampoTituloDoJogo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FCGCatalog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarUniqueAoCampoTituloDoJogo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ux_jogos_titulo",
+                table: "jogos",
+                column: "titulo",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ux_jogos_titulo",
+                table: "jogos");
+        }
+    }
+}
